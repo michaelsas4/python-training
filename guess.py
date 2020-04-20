752c0fded56ff9263e270d990556c45f211562e7
michaelsas4 <adeyeriayodele19@gmail.com>
2020-04-20 12:03:55 +0100

    :initial

--- guess.py
+++ guess.py
@@ -4,4 +4,14 @@ guess_count = 0
 guess_limit = 3
 out_of_guesses = False
 while guess != secret_num1 and not(out_of_guesses):
-    if guess_count < guess_limit
+    if guess_count < guess_limit:
+        guess = input("enter guess: ")
+        guess_count +=1
+    else:
+            out_of_guesses = True
+            if out_of_guesses:
+                print("out of guesses, You Lose! ")
+            else:
+                    print("You win! ")
+
+
