e4ecfff622a2d39270580305234f00566fccb6f3
michaelsas4 <adeyeriayodele19@gmail.com>
2020-07-30 20:35:49 +0100

    firstprogram

---
+++ client.py
@@ -0,0 +1,19 @@
+id1 = '845'
+id2 = '605'
+id3 = '305'
+id4 = '415'
+id5 = '505'
+client_id1 = {"845": "mike, no 2 ozumba mbadiwe, 35 years old, lagos state"}
+client_id2 = {"605": "sunday no 3 ozumba mbadiwe,55 years old, ikeja"}
+client_id3 = {"305": "sunday no 3 ozumba mbadiwe,65 years old, ikeja"}
+id1 = input("enter your id:")
+print(client_id1["845"])
+id2 = input("enter your id:")
+print(client_id2["605"])
+id2 = input("enter your id:")
+print("client_id3")
+
+
+
+
+
---
+++ clientdetails.py
@@ -0,0 +1,40 @@
+
+id1 = '845'
+id2 = '605'
+id3 = '305'
+id4 = '415'
+id5 = '505'
+client_id1 = {'name': 'John Mike', 'age': '40', 'place of birth': 'califonia', 'address': 'califonia CA1215'}
+client_id2 = {'name': 'michael bolton', 'age': '60', 'place of birth': 'larvegas', 'address': 'califonia CA1015'}
+client_id3 = {'name': 'stone jose', 'age': '40', 'place of birth': 'texas', 'address': 'califonia CA1215'}
+client_id4 = {'name': 'lord jone', 'age': '30', 'place of birth': 'califonia', 'address': 'califonia CA1215'}
+client_id15 = {'name': 'Tailor', 'age': '65', 'place of birth': 'califonia', 'address': 'califonia CA1215'}
+id = input('Enter your id: ')    
+if id == id1:  
+    print('client_id1')
+elif id == id2:
+    print('clinet_id2')
+elif id == id3:
+    print('clinet_id3')
+elif id == id4:
+    print('clinet_id4')
+elif id == id5:
+    print('clinet_id5')
+else:
+     print('you have entered invalid id')
+
+
+
+id1 = '845'
+id2 = '605'
+id3 = '305'
+id4 = '415'
+id5 = '505'
+client_id1 = {'name': 'John Mike', 'age': '40', 'place of birth': 'califonia', 'address': 'califonia CA1215'}
+client_id2 = {'name': 'michael bolton', 'age': '60', 'place of birth': 'larvegas', 'address': 'califonia CA1015'}
+client_id3 = {'name': 'stone jose', 'age': '40', 'place of birth': 'texas', 'address': 'califonia CA1215'}
+client_id4 = {'name': 'lord jone', 'age': '30', 'place of birth': 'califonia', 'address': 'califonia CA1215'}
+client_id15 = {'name': 'Tailor', 'age': '65', 'place of birth': 'califonia', 'address': 'califonia CA1215'}
+def client_id(id, client_id):
+    id = input('Enter your id: ')
+
